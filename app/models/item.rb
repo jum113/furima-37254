22bec9151610b77499a6a08c24b449d9c08a90f6
jum113 <japan_movieact@yahoo.co.jp>
2021-12-14 21:58:41 +0900

    ActiveHashの導入及びバリデーション、アソシエーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,25 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordAExtensions
+  belongs_to :genre
+  belongs_to :status
+  belongs_to :delivery_charge
+  belongs_to :delivery_area
+  belongs_to :delivery_date
+
   belongs_to :user
   has_one_attached :image
+
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :description
+  end
+  validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+  with_options presence: true, numericality: { other_than: 1, message: "can't be blank" } do
+    validates :genre_id
+    validates :status_id
+    validates :delivery_charge_id
+    validates :delivery_area_id
+    validates :delivery_date_id
+  end
 end
