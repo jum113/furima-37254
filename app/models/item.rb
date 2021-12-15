bc8fc7ebfe476d82915de2fa3a4e0bc9445a01b1
jum113 <japan_movieact@yahoo.co.jp>
2021-12-15 21:07:57 +0900

    商品出品機能のモデル単体テストの実施

--- app/models/item.rb
+++ app/models/item.rb
@@ -15,7 +15,7 @@ class Item < ApplicationRecord
     validates :description
   end
   validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
-  with_options presence: true, numericality: { other_than: 1, message: "can't be blank" } do
+  with_options numericality: { other_than: 1, message: "can't be blank" } do
     validates :genre_id
     validates :status_id
     validates :delivery_charge_id
