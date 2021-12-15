dbf6e5f388e29ee941bba30390b20c8a62c6f4ce
jum113 <japan_movieact@yahoo.co.jp>
2021-12-15 21:06:25 +0900

    JavaScriptの実装

---
+++ app/javascript/price.js
@@ -0,0 +1,14 @@
+function price(){
+  const itemPrice = document.getElementById("item-price");
+  itemPrice.addEventListener('keyup', () => {
+    const addTaxPrice = document.getElementById("add-tax-price");
+    const profit = document.getElementById("profit");
+    const value = itemPrice.value;
+    let tax = 10;
+    let charge = value / tax;
+    addTaxPrice.innerHTML = charge;
+    profit.innerHTML = value - charge;
+  });
+};
+
+window.addEventListener('load', price);
