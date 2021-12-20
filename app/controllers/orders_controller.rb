0f4e688a90a70dd372689ab529b9b8f233d5cdba
jum113 <japan_movieact@yahoo.co.jp>
2021-12-20 16:45:44 +0900

    Formオブジェクトの実装とエラーハンドリングの設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,23 @@
 class OrdersController < ApplicationController
   def index
-    
+    @item = Item.find(params[:item_id])
+    @order_delivery = OrderDelivery.new
+  end
+
+  def create
+    @order_delivery = OrderDelivery.new(order_params)
+    if @order_delivery.valid?
+      @order_delivery.save
+      redirect_to root_path
+    else
+      @item = Item.find(params[:item_id])
+      render :index
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_delivery).permit(:post_code, :delivery_area_id, :municipality, :address, :building, :telephone_number).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 end
--- app/models/order_delivery.rb
+++ app/models/order_delivery.rb
@@ -5,7 +5,13 @@ class OrderDelivery
   with_options presence: true do
     validates :user_id, :item_id
     validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
-    validates :delivery_area_id, numericality: { other_than: 0, message: "can't be blank" }
+    validates :delivery_area_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :municipality, :address
-    validates :telephone_number, format: { with: /\A[0-9]{10, 11}\z/, message: "is invalid. Exclude hyphen(-)" }
+    validates :telephone_number, format: { with: /\A[0-9]{10,11}\z/, message: "is invalid. Exclude hyphen(-)" }
   end
+
+  def save
+    order = Order.create(user_id: user_id, item_id: item_id)
+    Delivery.create(post_code: post_code, delivery_area_id: delivery_area_id, municipality: municipality, address: address, building: building, telephone_number: telephone_number, order_id: order.id)
+  end
+end
