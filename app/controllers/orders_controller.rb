d162a21167b0a769e7788194af69d960855f1114
jum113 <japan_movieact@yahoo.co.jp>
2021-12-21 17:53:21 +0900

    PAY.JPの実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,8 +5,10 @@ class OrdersController < ApplicationController
   end
 
   def create
+    @item = Item.find(params[:item_id])
     @order_delivery = OrderDelivery.new(order_params)
     if @order_delivery.valid?
+      pay_item
       @order_delivery.save
       redirect_to root_path
     else
@@ -18,6 +20,15 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order_delivery).permit(:post_code, :delivery_area_id, :municipality, :address, :building, :telephone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:order_delivery).permit(:post_code, :delivery_area_id, :municipality, :address, :building, :telephone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],
+      currency: 'jpy'
+    )
   end
 end
--- app/models/order_delivery.rb
+++ app/models/order_delivery.rb
@@ -1,6 +1,6 @@
 class OrderDelivery
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :post_code, :delivery_area_id, :municipality, :address, :building, :telephone_number
+  attr_accessor :user_id, :item_id, :post_code, :delivery_area_id, :municipality, :address, :building, :telephone_number, :token
 
   with_options presence: true do
     validates :user_id, :item_id
@@ -8,6 +8,7 @@ class OrderDelivery
     validates :delivery_area_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :municipality, :address
     validates :telephone_number, format: { with: /\A[0-9]{10,11}\z/, message: "is invalid. Exclude hyphen(-)" }
+    validates :token
   end
 
   def save
